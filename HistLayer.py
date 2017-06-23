77873da5f2b46f84f02026edb90fcc49a63bd0fb
lemonzeppyman <warriorfalcon@gmail.com>
2017-06-23 17:47:55 -0400

    V.0.1

--- HistLayer.py
+++ HistLayer.py
@@ -20,41 +20,12 @@ class HistLayer(nn.Module):
         self.histBins.sort()
         self.numBins = len(histBins)
 
-        #initialize input dimensions
-        # if type(D_in).__name__ == 'int':
-        #     self.D_in = [D_in, D_in]
-        # elif type(D_in).__name__ == 'list':
-        #     if len(D_in) == 2:
-        #         self.D_in = D_in
-        # else:
-        #     print("ERROR, UNACCEPTABLE INPUT DIMENSION")
         self.D_in = D_in
 
-        #initialize stride
-        # if type(stride).__name__ == 'int':
-        #     self.stride = [stride, stride]
-        # elif type(stride).__name__ == 'list':
-        #     if len(stride) == 2:
-        #         self.stride = stride
-        # else:
-        #     print("ERROR, UNACCEPTABLE STRIDE LENGTH")
         self.stride = stride
 
-        #initialize filter dimensions
-        # if type(filt_dim).__name__ == 'int':
-        #     self.filt_dim = tuple([filt_dim, filt_dim])
-        # elif type(filt_dim).__name__ == 'list' or 'tuple':
-        #     if len(filt_dim) == 2:
-        #         self.filt_dim = tuple(filt_dim)
-        # else:
-        #     print("ERROR, UNACCEPTABLE FILTER SIZE")
         self.filt_dim = filt_dim
 
-        #initialize padding size
-        # if type(padding).__name__ == 'int':
-        #     self.padding = padding
-        # else :
-        #     print("ERROR, UNACCEPTABLE PADDING")
         self.padding = padding
 
         #initialize padding value
@@ -62,8 +33,8 @@ class HistLayer(nn.Module):
 
         #determine output dimensions
         D_out = [0, 0]
-        D_out[0] = math.floor((D_in[0]-filt_dim[0] + 2*padding)/stride[0])+1
-        D_out[1] = math.floor((D_in[1]-filt_dim[1] + 2*padding)/stride[1])+1
+        D_out[0] = math.floor((D_in[0]-filt_dim[0] + 2*padding)/stride[0])+1 #output row dim
+        D_out[1] = math.floor((D_in[1]-filt_dim[1] + 2*padding)/stride[1])+1 #output column dim
 
         self.D_out = D_out
 
@@ -78,17 +49,20 @@ class HistLayer(nn.Module):
         #modeBin holds the bin value with the highest frequencies, holds float
 
         output = torch.IntTensor(self.filt_dim[0], self.filt_dim[1]).zero_()
-        modeVect = np.zeros((1, self.numBins), dtype=np.intp)
+        histFreq = np.zeros((self.numBins), dtype=np.intp)
         for i in range(0, self.filt_dim[0]):
             for j in range(0, self.filt_dim[1]):
                 for k in range(0, self.numBins):
-                    if img[i, j] < self.histBins[k]:
+                    # print("img["+str(i)+", "+ str(j) +"] = ",img[i,j])
+                    if img[i, j] <= self.histBins[k]:
+                        # print("histBin["+str(k)+"] = ", histBins[k])
                         output[i, j] = k
                         break
-                    modeVect[k] += 1
-        mode = np.max(modeVect)
-        modeBin = np.argmax(modeVect)
-        return output, mode, modeBin
+
+                histFreq[k] += 1
+        modeFreq = np.max(histFreq)
+        modeBin = np.argmax(histFreq)
+        return output, modeFreq, modeBin
 
 
     def forward(self, xx):
@@ -96,32 +70,38 @@ class HistLayer(nn.Module):
         p = self.padding #layer padding
         output = torch.FloatTensor(torch.zeros(self.D_out)) ##tensor for output
 
-
-
         ##pad input
+        ##inserting input into a larger array filled with pad values
+        ##not currently being used
         temp_in = torch.zeros([self.D_in[0]+p, self.D_in[1]+p])
         temp_in[p:self.D_in[0]+1, p:self.D_in[1]+1] = xx
 
         ##operate on padded input
         for i in range(0, self.D_in[0], self.stride[0]): #row/height loop
+            out_i = int(i / self.stride[0])
             for j in range(0, self.D_in[1], self.stride[1]): #column/width loop
+                out_j = int(j/self.stride[1])
                 temp_window = xx[i:i+filt_dim[0], j:j+filt_dim[1]] #isolate window
-                temp_binned, mode, modeBin = self.bin_img(temp_window)
-                print(output.size())
-                print(self.stride)
-                print(i), print(j)
-                output[i, j] = float(mode)
-                self.gradient[i, j, i:i+filt_dim[0], j:j+filt_dim[1]] = temp_binned.eq(int(mode))
+                temp_binned, mode_freq, modeBin = self.bin_img(temp_window)
+                # print(temp_binned, mode_freq, modeBin)
+                output[out_i, out_j] = float(modeBin)
+                self.gradient[out_i, out_j, i:i+filt_dim[0], j:j+filt_dim[1]] = temp_binned.eq(int(modeBin))
         return output
 
 
     def backward(self, grad_output):
-        ##need to multiply grad value by grad output.. poorly phrasedbut need to actually backprop.
-        output = torch.zeros(self.D_out[0], self.D_out[1], self.D_in[0], self.D_in[1]).register_buffer()
-        for i in range(0, self.D_in[1], self.stride[1]): #row/height loop
-            for j in range(0, self.D_in[0], self.stride[0]): #column/width loop
-               output[i,j] = self.gradient[i,j]*grad_output[i,j]
-        return output
+
+        #initialize backprop tensor
+        backprop = torch.zeros(self.D_out[0], self.D_out[1], self.D_in[0], self.D_in[1]) #float tensor
+        # print(self.gradient)
+        #for each previsouly outputted element
+        for i in range(0, self.D_in[0], self.stride[0]): #row/height loop
+            out_i = int(i / self.stride[0])
+            for j in range(0, self.D_in[1], self.stride[1]): #column/width loop
+                out_j = int(j / self.stride[1])
+                # print(grad_output[out_i,out_j], self.gradient[out_i,out_j])
+                backprop[out_i,out_j] = self.gradient[out_i,out_j].float()*grad_output[out_i,out_j]
+        return backprop
 
     def zero_grad(self):
         ##zero grad output
@@ -133,10 +113,19 @@ stride = [3, 5]
 filt_dim = [3, 5]
 histBins = [0.5, 1]
 
+testWin = torch.ones(filt_dim)
+
 input = torch.zeros(D_in)
 input[6:9,2:7] = 1.0
-# print(input)
-
 
 net = HistLayer(D_in, padding, stride, filt_dim, histBins)
-net.forward(input)
+# print(net.bin_img(testWin))
+
+
+# print(input)
+yy = net.forward(input)
+print((net.gradient.size()))
+# print(yy)
+zz = net.backward(yy)
+print(yy)
+print(zz[2,0])
